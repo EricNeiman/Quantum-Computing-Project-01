58c6bba205d7c06af7c209dd903c1c03652b1b16
Eric Neiman <ericneiman25@gmail.com>
2019-02-21 14:19:45 -0500

    Added some stuff

--- NeimanEric_Homework01.py
+++ NeimanEric_Homework01.py
@@ -1,13 +1,16 @@
-#Eric Neiman CMSC 491 Homework 01
-import numpy as np # using numpy for arrays
-
+import numpy as np
 
 class Register:
-    def__init__(self, k, m):
+    def __init__(self, m, k, infoBit):
         # m is the dimensionality of the vector
-        # k is the location of the 1 bit
+        # k is the location of the information bit
+        if m < 1 | m > 15:
+            print("The number of qubits must be between 1 and 15.")
+            quit()  # Quits the program due to a bad input
+        self.vector = np.zeros((2**m,), dtype=int)  # creates and zero array with a size of 2 to the power of m
+        print(self.vector)
 
+# class Operator:
+#     def __init__(self):
 
-
-class Operator:
-    def__init__(self):
+register = Register(2, 0, 0)
