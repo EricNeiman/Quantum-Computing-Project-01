905e898d0cf619c138e27e124873a7b92f680ab4
Eric Neiman <ericneiman25@gmail.com>
2019-02-21 00:43:21 -0500

    Setting up basic file structure

---
+++ NeimanEric_Homework01.py
@@ -0,0 +1,13 @@
+#Eric Neiman CMSC 491 Homework 01
+import numpy as np # using numpy for arrays
+
+
+class Register:
+    def__init__(self, k, m):
+        # m is the dimensionality of the vector
+        # k is the location of the 1 bit
+
+
+
+class Operator:
+    def__init__(self):
